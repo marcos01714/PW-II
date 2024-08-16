9077f2446dc9060cdcfeb387702960307567addd
marcos01714 <mv.sanchescardoso@gmail.com>
2024-08-15 22:30:47 -0300

    Aula de Variáveis

---
+++ bootstrap.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial do site</title>
+    <link href="bootstrap.min.css" rel="stylesheet"/>
+</head>
+<body>
+<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Aula PW 2</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Página Inicial</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="variavel.php">Variável</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="while.php">While</a></li>
+            <li><a class="dropdown-item" href="for.php">For</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="foreach.php">Foreach</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="bootstrap.php">Bootstrap</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" >Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    <h1>Aula bootstrap 09-08-24</h1>
+    <div class="container">
+
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4">
+            <div class="alert alert-success" role="alert">
+                Cadastro feito com sucesso!
+            </div>
+</div>
+</div> 
+
+    <button type="button" class="btn btn-primary">Salvar cadastro</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-link">Link</button>
+
+    <a href="www.google.com.br" class="btn btn-primary"> Acesse o Google aqui </a>
+
+    <table class="table table-striped table-bordered table-hover">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+        </tr>
+    </tbody>
+    </table>
+
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" >
+    <div class="form-text">Digite um email válido para acessar o sistema.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-success">Submit</button>
+</form>
+
+    <script src="bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- variavel.php
+++ variavel.php
@@ -48,8 +48,27 @@
   </div>
 </nav>
     <div class="container">
-      <h1>Página inicial do site</h1>
-    
+      <h1>Variáveis em PHP</h1>
+      <p> $Numero = 10; //Aqui é uma variável inteira. </p>
+      <p> $String = "Marcos camisa 10"; </p>
+      <p> $String1 = "Marcos camisa 10".$numero; </p>
+      <p> $String2 = "Marcos camisa 10 $numero"; </p>
+      <p> $ValorDecimal = 10.50; </p>
+      <p> $Ativo = true; </p>
+      <p> /* Aqui é um bloco de comentário */ </p>
+      <?php //Tudo que está entre as tags "PHP", não aparece na tela. ?>
+
+      <?php //A não ser que usemos a função echo com string.  ?>
+        <?php 
+            echo "Hello World"; 
+            echo "<br>";
+            $numero = 10;
+            $nome = "Marcos";
+            $frase1 = "<p> $nome camisa $numero Frase com parágrafo </p>";
+            $frase2 = $nome."  camisa  ".$numero; //Concatenação das antigas
+            echo $frase1;
+            echo "$frase2 É o que tem dentro da variável";
+        ?>
   </div>
     <script src="bootstrap.bundle.min.js"></script>
 </body>
